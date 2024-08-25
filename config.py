fc7e0b26d9bc3770ad17a2f7d7c6a06c217888d7
CHALCOGEN645 <166417113+CHALCOGEN645@users.noreply.github.com>
2024-08-25 18:31:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Xyz12341/Vip-music-plugin",
+    "https://github.com/CHALCOGEN645/Vip-music-plugin2",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Xyz12341/MOONMUSICc",
+    "https://github.com/CHALCOGEN645/MOONMUSICc",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
